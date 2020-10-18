875769697291cfe0df59d73620d10e8b014723e5
Erin A <eaannis@umich.edu>
2020-10-18 19:04:07 -0400

    Ok I think adding secrets is actually right this time to newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -1,7 +1,6 @@
 import requests
 import secrets as NEWSAPI_KEY
 
-#NEWSAPI_KEY = '2726118c98cd4c1abe7e8a0603b19f6c'
 base_url = 'https://newsapi.org/v2/top-headlines'
 params = {"apiKey": NEWSAPI_KEY, "q": "covid"}
 response = requests.get(base_url, params)
