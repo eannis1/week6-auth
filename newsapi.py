2123275453f9dc873659db87a2f4d07cba4a71ac
Erin A <eaannis@umich.edu>
2020-10-18 19:00:07 -0400

    Added secrets.py to newsapi.py (I think)

--- newsapi.py
+++ newsapi.py
@@ -1,6 +1,7 @@
 import requests
+import secrets.py as NEWSAPI_KEY
 
-NEWSAPI_KEY = '2726118c98cd4c1abe7e8a0603b19f6c'
+#NEWSAPI_KEY = '2726118c98cd4c1abe7e8a0603b19f6c'
 base_url = 'https://newsapi.org/v2/top-headlines'
 params = {"apiKey": NEWSAPI_KEY, "q": "covid"}
 response = requests.get(base_url, params)
@@ -9,3 +10,5 @@ articles_list = result['articles']
 
 for dict in articles_list:
     print(dict['source']['name'], dict['title'])
+
+print("Hello, World!")
